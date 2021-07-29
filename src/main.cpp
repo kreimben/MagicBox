6a9199ab646dd28278298787162b2e97f4881139
Ubuntu <ubuntu@ip-172-26-14-47.ap-northeast-2.compute.internal>
2021-07-29 03:20:07 +0000

    Delete redundant comments.

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,10 @@ int main() {
     /* Start event loop! */
     auto app = MBApp::getInstance();
 
-    // auto topBar = std::make_unique<MBTopBar>(app->getConnection(), app->getScreen(), app->getWindowID());
-
     /* Do Anything before execute eventLoop!!! */
     std::thread([app] {
         app->eventLoop();
     }).join();
-    /* After quit event loop, Disconnect the windostd::shared_ptr<w from X Ser> r. */
-    // app->disconnectApp();
-    std::cout << "Bye bye!" << std::endl;
 
     return 0;
 }
