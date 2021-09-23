85bc5fb500d1e9f2ed5d215d195df65ebddad7b2
Aksidion <aksidionkreimben@gmail.com>
2021-09-23 21:28:27 +0900

    Initial commit for swift version.

---
+++ Main.swift
@@ -0,0 +1,13 @@
+//
+// Created by kreimben on 9/23/21.
+//
+
+import Foundation
+import WaylandClient
+
+@main
+class Main {
+    static func main() {
+        print("Hello, This will be MagicBox!")
+    }
+}
---
+++ Package.swift
@@ -0,0 +1,20 @@
+//
+// Created by kreimben on 9/23/21.
+//
+// swift-tools-version:5.4
+import PackageDescription
+
+let package = Package(
+        name: "WaylandClient",
+        products: [
+            .library(name: "WaylandClient", targets: ["WaylandClient"]),
+        ],
+        dependencies: [
+          .library("wayland-client")
+        ],
+        targets: [
+            .target(name: "WaylandClient", dependencies: [
+                .product(name: "WaylandClient", package: "WaylandClient"),
+            ]),
+        ]
+)
